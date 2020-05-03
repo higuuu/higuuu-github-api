2ab04c9717606d01b9bc3dde990ea8039fbea956
higuzero <higuzeroggw@gmail.com>
2020-05-03 13:55:13 +0900

    [mod] databinding 使ったので setContentView はいらない

--- app/src/main/java/com/example/higuuugithubapi/main/MainActivity.kt
+++ app/src/main/java/com/example/higuuugithubapi/main/MainActivity.kt
@@ -1,12 +1,11 @@
-package com.example.higuuugithubapi
+package com.example.higuuugithubapi.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.higuuugithubapi.data.UserRepoRequest
+import com.example.higuuugithubapi.R
 import com.example.higuuugithubapi.databinding.ActivityMainBinding
 import com.example.higuuugithubapi.util.MainViewModelFactory
 import com.example.higuuugithubapi.util.RepositoryFactory
@@ -17,9 +16,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            DataBindingUtil.setContentView<ActivityMainBinding>(this,
+                R.layout.activity_main
+            )
 
         val factory = MainViewModelFactory(RepositoryFactory.createClientApiRepository())
 
